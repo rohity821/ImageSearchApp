52e738c98e38ece0e8a7febea1426c63333f00a1
rohity1011 <rohit.yadav@wynk.in>
2018-09-07 21:56:56 +0530

    Initial Code Architecture

---
+++ ImageSearch/ImageSearch/Interactor/ImageSearchInteractor.swift
@@ -0,0 +1,14 @@
+//
+//  ImageSearchInteractor.swift
+//  ImageSearch
+//
+//  Created by B0203596 on 07/09/18.
+//  Copyright © 2018 Rohit. All rights reserved.
+//
+
+import Foundation
+
+
+class ImageSearchInteractor  {
+
+}
---
+++ ImageSearch/ImageSearch/Models/ImageDataModel.swift
@@ -0,0 +1,9 @@
+//
+//  ImageDataModel.swift
+//  ImageSearch
+//
+//  Created by B0203596 on 07/09/18.
+//  Copyright © 2018 Rohit. All rights reserved.
+//
+
+import Foundation
